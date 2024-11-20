710dd0847920c6a583cea02a9668d8a1e4700c17
Kelin Argueta <kelinargueta@icloud.com>
2024-11-20 14:11:10 -0500

    no message

--- Shiny/app.R
+++ Shiny/app.R
@@ -8,62 +8,62 @@ library(dbplyr)
 library(ProtResDash)
 library(tidyr)
 library(stringr)
-
-library(ComplexHeatmap)
-library(colorRamp2)
-
 library(tidyr)
 library(stringr)
 library(ggplot2)
+library(yaml)
+
 
 #Box plot
-file.path(here::here(), "R", "boxPlot.R") |>
-  source()
+file.path(here::here(), "R", "boxPlot.R") |> source()
 #Box plot
-file.path(here::here(), "R", "boxPlot2.R") |>
-  source()
-
+file.path(here::here(), "R", "boxPlot2.R") |> source()
 #Bar plot
- file.path(here::here(), "R", "barPlot.R") |>
-   source()
- 
-#Heatmap
-file.path(here::here(), "R", "heatMap.R") |>
- source()
+file.path(here::here(), "R", "barPlot.R") |> source()
+
+config <- yaml::read_yaml("config.yaml", eval.expr = TRUE)
+
 
 # move default data to sqlite file
 extdata <- system.file("extdata", package = 'ProtResDash')
 
-if(!file.exists("data/protein_peptidedb.sqlite")) {
+
+#config
+#if(!file.exists("data/protein_peptidedb.sqlite")) {
+if(!file.exists(config$database$file)) {
+  
   import_raw ( file.path(extdata, "peptides.txt"), 
                file.path(extdata, "proteinGroups.txt"), 
-               "data/protein_peptidedb.sqlite")
+               config$database$file)
+               #"data/protein_peptidedb.sqlite")
 }
-
-con <- dbConnect(SQLite(), dbname = "data/protein_peptidedb.sqlite")
+#config
+#con <- dbConnect(SQLite(), dbname = "data/protein_peptidedb.sqlite")
+con <- dbConnect(SQLite(), dbname = config$database$file)
 peptides <- tbl(con, "peptides")
 proteins <- tbl(con, "proteins")
 
-
-#bslib theme
-base_theme <- bs_theme(bootswatch = "quartz")
+#UI bslib theme
+#config
+#base_theme <- bs_theme(bootswatch = "quartz")
+base_theme <- bs_theme(bootswatch = config$themes$base_theme)
 
 protein_theme <- bs_add_rules(base_theme,
                               ".protein-theme { 
-    background-color: #f8f9fa; 
-    padding: 10px; 
-    border-radius: 5px; 
-    box-shadow: 0 4px 8px rgba(0,0,0,0.1); 
-  }"
+      background-color: ", config$themes$protein_theme$background_color, "; 
+      padding: ", config$themes$protein_theme$padding, "; 
+      border-radius: ", config$themes$protein_theme$border_radius, "; 
+      box-shadow: ", config$themes$protein_theme$box_shadow, "; 
+    }"
 )
 
 peptide_theme <- bs_add_rules(base_theme,
                               ".peptide-theme { 
-    background-color: #e9ecef; 
-    padding: 10px; 
-    border-radius: 5px; 
-    box-shadow: 0 4px 8px rgba(0,0,0,0.1); 
-  }"
+      background-color: ", config$themes$peptide_theme$background_color, "; 
+      padding: ", config$themes$peptide_theme$padding, "; 
+      border-radius: ", config$themes$peptide_theme$border_radius, "; 
+      box-shadow: ", config$themes$peptide_theme$box_shadow, "; 
+    }"
 )
 
 #UI
@@ -106,7 +106,7 @@ ui <- div(
     )
   ),
   
-  #QC Tab with plots: PCA, Box, and Bar
+  #QC Tab with plots
   tabPanel(
     "QC",
     fluidRow(
@@ -130,11 +130,6 @@ ui <- div(
         h3("Bar Plot"),
         plotOutput("barPlot")
       ),
-       column(
-         width = 6,
-         h3("Heatmap"),
-         plotOutput("heatMap")
-       )
      )
     )
   )
@@ -147,83 +142,68 @@ server <- function(input, output, session) {
   output$proteinTable <- renderDT({
      req(proteins)
     
-    #remember to generalize this part
-    protein_data <- collect(select(proteins, c('Sequence','Mass','Proteins', 
-                                         'Reporter.intensity.corrected.1',
-                                         'Reporter.intensity.corrected.2',
-                                         'Reporter.intensity.corrected.3',
-                                         'Reporter.intensity.corrected.4',
-                                         'Reporter.intensity.corrected.5',
-                                         'Reporter.intensity.corrected.6',
-                                         'Reporter.intensity.corrected.7',
-                                         'Reporter.intensity.corrected.8',
-                                         'Reporter.intensity.corrected.9',
-                                         'Reporter.intensity.corrected.10')))  
+  #   protein_data <- collect(select(proteins, c("Sequence", "Mass", "Proteins",
+  #               reporter_intensity_columns("Reporter.intensity.corrected", 1:10))))
+  #   datatable(protein_data, selection = 'single', options = list(pageLength = 4))
+  # })
+    
+    #config
+    protein_data <- collect(select(proteins, config$columns$protein_table))
     datatable(protein_data, selection = 'single', options = list(pageLength = 4))
   })
-  
-  
-  
+    
   #Peptide table
   output$peptideTable <- renderDT({
     req(input$proteinTable_rows_selected)
     
-    
       selected_row <- input$proteinTable_rows_selected
-      
       selectedProteinID <- proteins %>%
       filter(row_number() == selected_row) %>% 
       pull(Protein.group.IDs) #Extract the Protein.group.IDs from the selected row.
-      
     
     selectedPeptides <- peptides %>%  #remember to generalize this part
       filter(Protein.group.IDs == !!selectedProteinID[1]) %>%
       collect() #Collect the data as a df
-      datatable(select(selectedPeptides, c('Protein.group.IDs',
-                                           'Reporter.intensity.corrected.1',
-                                           'Reporter.intensity.corrected.2',
-                                           'Reporter.intensity.corrected.3',
-                                           'Reporter.intensity.corrected.4',
-                                           'Reporter.intensity.corrected.5',
-                                           'Reporter.intensity.corrected.6',
-                                           'Reporter.intensity.corrected.7',
-                                           'Reporter.intensity.corrected.8',
-                                           'Reporter.intensity.corrected.9',
-                                           'Reporter.intensity.corrected.10',
-                                           'Mod..peptide.IDs')), 
-                
-  selection = 'single', options = list(pageLength = 4))
+    
+  #config
+  #   datatable(select(selectedPeptides, c("Protein.group.IDs", "Mod..peptide.IDs"),
+  #           reporter_intensity_columns("Reporter.intensity.corrected", 1:10)),
+  #           selection = 'single', options = list(pageLength = 4))
+  # })
+    datatable(select(selectedPeptides, config$columns$peptide_table), 
+      selection = 'single', options = list(pageLength = 4))
   })
-  
-  #PCA Plot#
+    
+    
+  #PCA Plot
   output$pcaPlot <- renderPlot ({
     data <- proteins %>%
       select(starts_with('Reporter.intensity.corrected')) %>%
       select(1:10) |>
-      collect()  #generalize 
-        
-    if (nrow(data) > 1 && ncol(data) > 1) 
+      collect()
+
+    if (nrow(data) > 1 && ncol(data) > 1)
     {
       pca_res <- prcomp(data, center = TRUE, scale. = TRUE)
       pca_df <- as.data.frame(pca_res$x[, 1:2]) # Extract the first two principal components and convert to a data frame
-        
+
       pcaPlot(data,
               rep(c('Control', 'IgM'), each = 5))
     }else{
       plot(NA, NA, xlim = 0:1, ylim = 0:1, type = "n", xlab = "",
            main = "Not enough data for PCA")
     }
-  }) 
-      
+  })
+
     #Box Plot 2: No-normalized
   output$boxPlot2 <- renderPlot ({
     data <- proteins %>%
       select(starts_with('Reporter.intensity')) %>%
       select(11:20) |>
-      collect() 
-          
+      collect()
+
     boxPlot2(data)
-  } ) 
+  } )
   
   #Box Plot: Normalized
   output$boxPlot <- renderPlot ({
@@ -244,21 +224,6 @@ server <- function(input, output, session) {
     
     barPlot(data)
   } ) 
-  
-  #ComplexHeatmap
-  output$heatMap <- renderPlot({
-   data_matrix <- proteins %>%
-    select(starts_with('Reporter.intensity.corrected')) %>%
-    select(1:10) |>
-    collect() |> 
-    as.matrix()
-   
-   sub_mat <- data_matrix[1:10, 1:10] #take first 10 rows & col from data_matrix
-   #we convert the data -> matrix, then we create a sub_matrix -> display heatmap of sub_matrix
-    
-    heatMap(sub_mat)  
-  })
-
 } 
 
 shinyApp(ui, server) 
