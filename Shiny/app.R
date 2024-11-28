6b0c5118e73f76ee0263b3cd7933b0fd5b4850eb
Kelin Argueta <kelinargueta@icloud.com>
2024-11-28 16:16:08 -0500

    no message

--- Shiny/app.R
+++ Shiny/app.R
@@ -1,4 +1,4 @@
-#Libraries
+
 library(shiny)
 library(DT)
 library(bslib)
@@ -8,227 +8,187 @@ library(dbplyr)
 library(ProtResDash)
 library(tidyr)
 library(stringr)
-library(tidyr)
-library(stringr)
 library(ggplot2)
 library(yaml)
 
+# YAML configuration
+config <- yaml::read_yaml("config.yaml")
 
-#Box plot
+# Box plot
 file.path(here::here(), "R", "boxPlot.R") |> source()
-#Box plot
+# Box plot
 file.path(here::here(), "R", "boxPlot2.R") |> source()
-#Bar plot
+# Bar plot
 file.path(here::here(), "R", "barPlot.R") |> source()
 
-config <- yaml::read_yaml("config.yaml", eval.expr = TRUE)
+# Move default data to SQLite file if it doesn't exist
+extdata <- system.file("extdata", package = config$database$extdata$package)
 
-
-# move default data to sqlite file
-extdata <- system.file("extdata", package = 'ProtResDash')
-
-
-#config
-#if(!file.exists("data/protein_peptidedb.sqlite")) {
-if(!file.exists(config$database$file)) {
-  
-  import_raw ( file.path(extdata, "peptides.txt"), 
-               file.path(extdata, "proteinGroups.txt"), 
-               config$database$file)
-               #"data/protein_peptidedb.sqlite")
+if (!file.exists(config$database$path)) {
+  import_raw(file.path(extdata, config$database$extdata$peptides_file),
+             file.path(extdata, config$database$extdata$protein_groups_file),
+             config$database$path)
 }
-#config
-#con <- dbConnect(SQLite(), dbname = "data/protein_peptidedb.sqlite")
-con <- dbConnect(SQLite(), dbname = config$database$file)
+
+# Database connection
+con <- dbConnect(SQLite(), dbname = config$database$path)
 peptides <- tbl(con, "peptides")
 proteins <- tbl(con, "proteins")
 
-#UI bslib theme
-#config
-#base_theme <- bs_theme(bootswatch = "quartz")
-base_theme <- bs_theme(bootswatch = config$themes$base_theme)
+# bslib theme
+base_theme <- bs_theme(bootswatch = config$app$theme)
 
-protein_theme <- bs_add_rules(base_theme,
-                              ".protein-theme { 
-      background-color: ", config$themes$protein_theme$background_color, "; 
-      padding: ", config$themes$protein_theme$padding, "; 
-      border-radius: ", config$themes$protein_theme$border_radius, "; 
-      box-shadow: ", config$themes$protein_theme$box_shadow, "; 
-    }"
-)
+protein_theme <- bs_add_rules(base_theme, paste0(
+  ".protein-theme {",
+  paste(
+    paste0(names(config$app$base_theme_rules$protein), ": ", 
+                 config$app$base_theme_rules$protein, ";"),
+    collapse = " "
+  ),
+  "}"
+))
 
-peptide_theme <- bs_add_rules(base_theme,
-                              ".peptide-theme { 
-      background-color: ", config$themes$peptide_theme$background_color, "; 
-      padding: ", config$themes$peptide_theme$padding, "; 
-      border-radius: ", config$themes$peptide_theme$border_radius, "; 
-      box-shadow: ", config$themes$peptide_theme$box_shadow, "; 
-    }"
-)
+peptide_theme <- bs_add_rules(base_theme, paste0(
+  ".peptide-theme {",
+  paste(
+    paste0(names(config$app$base_theme_rules$peptide), ": ", 
+           config$app$base_theme_rules$peptide, ";"),
+    collapse = " "
+  ),
+  "}"
+))
 
-#UI
+# UI
 ui <- div(
   style = "overflow-x: scroll; width:100%; max-width:100%;",
   navbarPage(
-  theme = base_theme,
-  title = "Protein and Peptide Viewer",
-  
-  tabPanel(
-    "Home",
-    fluidRow(
-      column(
-        width = 6,
-        h4("Select a Protein:"),
-        tags$div(
-          class = "protein-theme",
-          DTOutput("proteinTable"),
-          tags$style(HTML("
-            .dataTables_filter input {
-              width: 50% !important;
-              overflow: scroll;
-            }
-           .shiny-input-container{
-           color: #474747;
-           }
-          "))
+    theme = base_theme,
+    title = config$app$title,
+    
+    # Home Tab
+    tabPanel(
+      config$ui$tabs[[1]]$name,
+      fluidRow(
+        column(
+          width = config$ui$tabs[[1]]$layout$fluidRow[[1]]$column,
+          h4("Select a Protein:"),
+          tags$div(
+            class = "protein-theme",
+            DTOutput("proteinTable")
+          )
         )
-      )),
-    fluidRow(
-      column(
-        width = 6,
-        h3("Peptide Description:"),
-        tags$div(
-          class = "peptide-theme",
-          DTOutput("peptideTable")
-         
+      ),
+      fluidRow(
+        column(
+          width = config$ui$tabs[[1]]$layout$fluidRow[[2]]$column,
+          h3("Peptide Description:"),
+          tags$div(
+            class = "peptide-theme",
+            DTOutput("peptideTable")
+          )
         )
       )
-    )
-  ),
-  
-  #QC Tab with plots
-  tabPanel(
-    "QC",
-    fluidRow(
-      column(
-        width = 6,
-        h3("Box plot: Normalized"),
-        plotOutput("boxPlot")
-      ),
-      column(
-        width = 6,
-        h3("Box plot 2: No-normalized"),
-        plotOutput("boxPlot2")
-      ),
-      column(
-        width = 6,
-        h3("PCA Plot"),
-        plotOutput("pcaPlot")
-      ),
-      column(
-        width = 6,
-        h3("Bar Plot"),
-        plotOutput("barPlot")
-      ),
-     )
+    ),
+    
+    # QC Tab
+    tabPanel(
+      config$ui$tabs[[2]]$name,
+      fluidRow(
+        lapply(config$plots, function(plot) {
+          column(
+            width = 6,
+            h3(plot$type),
+            plotOutput(plot$type)
+          )
+        })
+      )
     )
   )
 )
 
-#Server
+# Server
 server <- function(input, output, session) {
   
-  #Protein table
+  # Protein Table
   output$proteinTable <- renderDT({
-     req(proteins)
+    req(proteins)
     
-  #   protein_data <- collect(select(proteins, c("Sequence", "Mass", "Proteins",
-  #               reporter_intensity_columns("Reporter.intensity.corrected", 1:10))))
-  #   datatable(protein_data, selection = 'single', options = list(pageLength = 4))
-  # })
-    
-    #config
-    protein_data <- collect(select(proteins, config$columns$protein_table))
-    datatable(protein_data, selection = 'single', options = list(pageLength = 4))
+    protein_data <- collect(select(proteins, all_of(config$tables$protein_table$columns)))
+    datatable(protein_data, selection = 'single', options = list(pageLength = config$tables$protein_table$page_length))
   })
-    
-  #Peptide table
+  
+  # Peptide Table
   output$peptideTable <- renderDT({
     req(input$proteinTable_rows_selected)
     
-      selected_row <- input$proteinTable_rows_selected
-      selectedProteinID <- proteins %>%
-      filter(row_number() == selected_row) %>% 
-      pull(Protein.group.IDs) #Extract the Protein.group.IDs from the selected row.
+    selected_row <- input$proteinTable_rows_selected
+    selectedProteinID <- proteins %>%
+    filter(row_number() == selected_row) %>%
+    pull(Protein.group.IDs) # Extract the Protein.group.IDs from the selected row
     
-    selectedPeptides <- peptides %>%  #remember to generalize this part
-      filter(Protein.group.IDs == !!selectedProteinID[1]) %>%
-      collect() #Collect the data as a df
+    selectedPeptides <- peptides %>%
+    filter(Protein.group.IDs == !!selectedProteinID[1]) %>%
+    collect() # Collect the data as a df
     
-  #config
-  #   datatable(select(selectedPeptides, c("Protein.group.IDs", "Mod..peptide.IDs"),
-  #           reporter_intensity_columns("Reporter.intensity.corrected", 1:10)),
-  #           selection = 'single', options = list(pageLength = 4))
-  # })
-    datatable(select(selectedPeptides, config$columns$peptide_table), 
-      selection = 'single', options = list(pageLength = 4))
+    datatable(select(selectedPeptides, all_of(config$tables$peptide_table$columns)),
+    selection = 'single', options = list(pageLength = config$tables$peptide_table$page_length))
   })
-    
-    
-  #PCA Plot
-  output$pcaPlot <- renderPlot ({
+  
+  # PCA Plot
+  output$pcaPlot <- renderPlot({
     data <- proteins %>%
-      select(starts_with('Reporter.intensity.corrected')) %>%
-      select(1:10) |>
+      select(starts_with(config$plots$pca_plot$data_columns$starts_with)) %>%
+      select(config$plots$pca_plot$range[1]:config$plots$pca_plot$range[2]) %>%
       collect()
-
-    if (nrow(data) > 1 && ncol(data) > 1)
-    {
+    
+    if (nrow(data) > 1 && ncol(data) > 1) {
+      print("Not enough data for PCA")
       pca_res <- prcomp(data, center = TRUE, scale. = TRUE)
-      pca_df <- as.data.frame(pca_res$x[, 1:2]) # Extract the first two principal components and convert to a data frame
-
-      pcaPlot(data,
-              rep(c('Control', 'IgM'), each = 5))
-    }else{
+      pca_df <- as.data.frame(pca_res$x[, 1:2]) # Here it extract the first 2 principal components and convert it to a df
+      
+      pcaPlot(data, 
+              rep(c("Control", "IgM"), each = 5))
+    } else {
+      print("Enough data for PCA")
       plot(NA, NA, xlim = 0:1, ylim = 0:1, type = "n", xlab = "",
            main = "Not enough data for PCA")
     }
   })
-
-    #Box Plot 2: No-normalized
-  output$boxPlot2 <- renderPlot ({
-    data <- proteins %>%
-      select(starts_with('Reporter.intensity')) %>%
-      select(11:20) |>
-      collect()
-
-    boxPlot2(data)
-  } )
   
-  #Box Plot: Normalized
-  output$boxPlot <- renderPlot ({
+  # Box Plot: Normalized
+  output$boxPlot <- renderPlot({
     data <- proteins %>%
-      select(starts_with('Reporter.intensity.corrected')) %>%
-      select(1:10) |>
-      collect() 
+      select(starts_with(config$plots$box_plot$data_columns$starts_with)) %>%
+      select(config$plots$box_plot$range[1]:config$plots$box_plot$range[2]) |>
+      collect()
     
     boxPlot(data)
-  } ) 
+  })
   
-  #Bar Plot#
-  output$barPlot <- renderPlot ({
+  # Box Plot 2: No-normalized 
+  output$boxPlot2 <- renderPlot({
     data <- proteins %>%
-      select(starts_with('Reporter.intensity.corrected')) %>%
-      select(1:10) |>
-      collect() 
+      select(starts_with(config$plots$box_plot2$data_columns$starts_with)) %>%
+      select(config$plots$box_plot2$range[1]:config$plots$box_plot2$range[2]) |>
+      collect()
+    
+    boxPlot2(data)
+  })
+  
+  # Bar Plot
+  output$barPlot <- renderPlot({
+    data <- proteins %>%
+      select(starts_with(config$plots$bar_plot$data_columns$starts_with)) %>%
+      select(config$plots$bar_plot$range[1]:config$plots$bar_plot$range[2]) |>
+      collect()
     
     barPlot(data)
-  } ) 
-} 
+  })
+}
 
-shinyApp(ui, server) 
-  
-
+shinyApp(ui, server)
 
 
-  
+
+
+
